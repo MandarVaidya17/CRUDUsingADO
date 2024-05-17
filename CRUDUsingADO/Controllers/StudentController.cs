ecb361596d7d829fef7a984bbfc2bd47f3100443
MandarVaidya17 <mandarvaidya1711@gmail.com>
2024-05-17 13:02:59 +0530

    Add project files.

---
+++ CRUDUsingADO/Controllers/StudentController.cs
@@ -0,0 +1,123 @@
+﻿using CRUDUsingADO.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDUsingADO.Controllers
+{
+    public class StudentController : Controller
+    {
+        StudentDAL studdal;
+        private readonly IConfiguration configuration;
+
+        public StudentController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            studdal= new StudentDAL(this.configuration);
+        }
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            var model = studdal.GetStudents();
+            return View(model);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = studdal.GetStudentByID(id);
+            return View(model);
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student stud)
+        {
+            try
+            {
+                int result = studdal.AddStudent(stud);
+                if (result >= 1)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.ErrorMsg = "Something went wrong";
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var stud = studdal.GetStudentByID(id);
+            return View(stud);
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Student stud)
+        {
+            try
+            {
+                int result = studdal.EditStudent(stud);
+                if (result >= 1) { 
+                return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.ErrorMsg = "Something went wrong";
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var stud = studdal.GetStudentByID(id);
+            return View(stud);
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirm(int id)
+        {
+            try
+            {
+                int result = studdal.DeleteStudent(id);
+                if (result >= 1)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    ViewBag.ErrorMsg = "Something went wrong";
+                    return View();
+                }
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CRUDUsingADO/Models/StudentDAL.cs
@@ -0,0 +1,108 @@
+﻿using System.Data.SqlClient;
+
+namespace CRUDUsingADO.Models
+{
+    public class StudentDAL
+    {
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataReader dr;
+        private readonly IConfiguration configuration;
+
+        public StudentDAL(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+            string connstr = this.configuration.GetConnectionString("DefaultConnection");
+            con=new SqlConnection(connstr);
+        }
+
+        public List<Student> GetStudents()
+        {
+            List<Student> studentlist = new List<Student>();
+            string qry = "select * from student";
+            cmd=new SqlCommand(qry, con);
+            con.Open();
+            dr= cmd.ExecuteReader();
+            if(dr.HasRows)
+            {
+                while(dr.Read())
+                {
+                    Student student = new Student();
+                    student.Id = Convert.ToInt32(dr["id"]);
+                    student.Name = dr["name"].ToString();
+                    student.Age = Convert.ToInt32(dr["age"]);
+                    student.Address = dr["address"].ToString();
+                    studentlist.Add(student);   
+                    
+                }
+            }
+            con.Close();
+            return studentlist;
+        }
+
+        public int AddStudent(Student stud)
+        {
+            int result = 0;
+            string qry = "insert into student values(@id,@name,@age,@address)";
+            cmd=new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", stud.Id);
+            cmd.Parameters.AddWithValue("@name", stud.Name);
+            cmd.Parameters.AddWithValue("@age", stud.Age);
+            cmd.Parameters.AddWithValue("@address", stud.Address);
+            con.Open();
+            result=cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        public int EditStudent(Student stud)
+        {
+            int result = 0;
+            string qry = "update student set name=@name,age=@age,address=@address where id=@id";
+            cmd = new SqlCommand(qry, con);
+            
+            cmd.Parameters.AddWithValue("@name", stud.Name);
+            cmd.Parameters.AddWithValue("@age", stud.Age);
+            cmd.Parameters.AddWithValue("@address", stud.Address);
+            cmd.Parameters.AddWithValue("@id", stud.Id);
+            con.Open();
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+        public int DeleteStudent(int id)
+        {
+            int result = 0;
+            string qry = "delete from student where id=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            
+            con.Open();
+            result = cmd.ExecuteNonQuery();
+            con.Close();
+            return result;
+        }
+
+        public Student GetStudentByID(int id)
+        {
+            Student student = new Student();
+            string qry = "select * from student where id=@id";
+            cmd = new SqlCommand(qry, con);
+            cmd.Parameters.AddWithValue("@id", id);
+            con.Open();
+            dr= cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    student.Id = Convert.ToInt32(dr["id"]);
+                    student.Name = dr["name"].ToString();
+                    student.Age = Convert.ToInt32(dr["age"]);
+                    student.Address = dr["address"].ToString();
+                }
+            }
+            con.Close();
+            return student;
+        }
+
+    }
+}
